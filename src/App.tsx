226cdd2b9b804a7edc124c2f66f4df4d3d2af876
vimtage <vimtage1@gmail.com>
2023-11-02 15:45:20 -0300

    update build.yml

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <>
       <h1>build and deploy with github action on push</h1>
+      <p>{import.meta.env.VITE_PAGE_TITLE}</p>
+
+      <h2>API KEY</h2>
+      <p>{import.meta.env.API_KEY}</p>
+
+      <h2>FIREBASE KEY</h2>
+      <p>{import.meta.env.FIREBASE_KEY}</p>
+
     </>
   )
 }
